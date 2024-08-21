7d6b07b447e33d4daa8e7c1ea81b78ec49ef4e1b
Mohib <mohibullahmohim2020@gmail.com>
2024-08-21 19:34:08 +0600

    My Tabs added for about section

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import Layout from './Layout/Layout.jsx'
+import Home from './Pages/Home/Home.jsx';
+import ErrorPage from './Pages/ErrorPage/ErrorPage.jsx';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    errorElement : <ErrorPage/>,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Home />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Layout />
+    <RouterProvider router={router} />
+    
   </StrictMode>,
 )
