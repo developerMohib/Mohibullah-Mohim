634aa26f9231c7184d115f7bc085e759abf65115
Mohib <mohibullahmohim2020@gmail.com>
2024-08-21 18:48:09 +0600

    Navbar added with responsive

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -3,13 +3,13 @@ import Navbar from "../Components/Navbar/Navbar";
 import Home from "../Pages/Home/Home";
 
 const Layout = () => {
-    return (
-        <div>
-            <Navbar/>
-            <Home/>
-            <Footer/>
-        </div>
-    );
+  return (
+    <div>
+      <Navbar />
+      <Home />
+      <Footer />
+    </div>
+  );
 };
 
 export default Layout;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,18 +1,19 @@
-
-import About from '../../Components/About/About';
-import Contact from '../../Components/Contact/Contact';
-import Resume from '../../Components/Resume/Resume';
-import Service from '../../Components/Service/Service';
+import About from "../../Components/About/About";
+import Banner from "../../Components/Banner/Banner";
+import Contact from "../../Components/Contact/Contact";
+import Resume from "../../Components/Resume/Resume";
+import Service from "../../Components/Service/Service";
 
 const Home = () => {
-    return (
-        <div>
-           <Resume/>
-           <About/>
-           <Service/>
-           <Contact/>
-        </div>
-    );
+  return (
+    <div className="container mx-auto px-6 py-4">
+      <Banner />
+      <Resume />
+      <About />
+      <Service />
+      <Contact />
+    </div>
+  );
 };
 
 export default Home;
